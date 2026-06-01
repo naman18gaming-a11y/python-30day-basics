dd6eec5285793c8d027ea4552600cea55488f92b
naman yadav <naman18gaming@gmail.com>
2026-06-01 09:17:58 +0530

    Day 7: Difference in sets program

---
+++ day7datastrucures/diffinset.py
@@ -0,0 +1,8 @@
+# diffrencee in sets
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+difrrence_sets = A.difference(B)
+print("the diffrence of set  A and B is :", difrrence_sets)
+difrrence_sets = B.difference(A)
+print("the diffrence of set  A and B is :", difrrence_sets)
