88bda13b9da0cfec0c8a06828e050729ea9a83d0
naman yadav <naman18gaming@gmail.com>
2026-06-01 08:58:02 +0530

    Day 7: Union & Intersection program

---
+++ day7datastrucures/unioninter.py
@@ -0,0 +1,7 @@
+# Union & Intersection
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+union_set = A|B
+intersection_sets = A.intersection(B)
+print("union of A and B is :", union_set)
+print("intersection of A and B is :", intersection_sets )
