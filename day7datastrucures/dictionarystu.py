d47818f06752fc69d4bdfdd17a78065b528819a4
naman yadav <naman18gaming@gmail.com>
2026-06-02 12:04:19 +0530

    Day 7: Student dictionary program with add & update

--- day7datastrucures/dictionarystu.py
+++ day7datastrucures/dictionarystu.py
@@ -1,5 +1,15 @@
-#Student Dictionary
-student = {"name": "naman", "AGE" : "20" , "id" : "nigu"}
-print("NAME : ", student["name"])
-print("AGE: ", student["AGE"])
+# Student Dictionary
+student = {"name": "naman", "AGE": "20", "id": "nigu"}
+
+# Print original values
+print("NAME:", student["name"])
+print("AGE:", student["AGE"])
 print("ID:", student["id"])
+
+
+student["cgpa"] = "8.5"   # adding cgpa
+
+student["AGE"] = "21"     # updating age
+
+print("\nUpdated Student Dictionary:")
+print(student)
