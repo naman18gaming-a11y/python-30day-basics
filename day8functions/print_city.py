53986bd7c271139c500930ae54fdc1f939db2677
naman yadav <naman18gaming@gmail.com>
2026-06-03 15:00:38 +0530

    Day 8: print_city program

---
+++ day8functions/print_city.py
@@ -0,0 +1,7 @@
+# create a function that prints city name
+def print_city_name(city):
+    print("The city name is:", city)
+    greeting = "Welcome to the city of " + city
+    print(greeting)
+
+print_city_name("Delhi")
