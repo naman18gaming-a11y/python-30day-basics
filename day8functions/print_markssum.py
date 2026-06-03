ac98add881a24218e06dc41e596dfc3d69acc839
naman yadav <naman18gaming@gmail.com>
2026-06-03 15:20:38 +0530

    Day 8: print_markssum program

---
+++ day8functions/print_markssum.py
@@ -0,0 +1,7 @@
+# Function to print subject and marks
+def print_subject_marks(subject, marks):
+    print("Subject:", subject)
+    print("Marks:", marks)
+
+print_subject_marks("Math", 95)
+print_subject_marks("Physics", 88)
