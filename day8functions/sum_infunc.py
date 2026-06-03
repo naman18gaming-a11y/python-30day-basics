912475c2fa7f375ebe7fa1ddaa038b62c297c46a
naman yadav <naman18gaming@gmail.com>
2026-06-03 14:55:27 +0530

    Day 8: sum_infunc program

---
+++ day8functions/sum_infunc.py
@@ -0,0 +1,6 @@
+#Create a function that accepts two numbers and prints their sum.
+def sum_numbers(num1, num2):
+    total = num1 + num2
+    print("the sum of the two numbers is:", total )
+#Call the function with two numbers of your choice.
+print(sum_numbers(5, 10))
