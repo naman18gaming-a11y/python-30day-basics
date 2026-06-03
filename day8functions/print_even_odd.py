c77bc36183c8eb4a9e7bcf444f0312c0a9ce0a7d
naman yadav <naman18gaming@gmail.com>
2026-06-03 15:26:13 +0530

    Day 8: print_even_odd program

---
+++ day8functions/print_even_odd.py
@@ -0,0 +1,9 @@
+# tell If a number is even or odd
+def print_even_odd(num):
+    if num % 2 == 0:
+        print(num, "is an even number.")
+    else:
+        print(num, "is an odd number.")
+        
+print_even_odd(7)
+print_even_odd(8)
