4ca513fe1e019adb5f24f2e3f29c8224939ac997
naman yadav <naman18gaming@gmail.com>
2026-06-03 13:22:12 +0530

    Day 8: parameter_func program

---
+++ day8functions/parameter_func.py
@@ -0,0 +1,5 @@
+# Create a function that accepts a name and prints:
+def greet(name):
+    print("Hello, " + name + "! Welcome to the world of Python programming.")
+name_input = input("Enter your name: ")
+greet(name_input)   
