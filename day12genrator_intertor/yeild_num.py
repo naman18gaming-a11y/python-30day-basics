ece7387d96cbcdbd2fd7001acf3b10c073c9af4f
naman yadav <naman18gaming@gmail.com>
2026-06-07 19:31:13 +0530

    Day 12: yield numbers using generator

---
+++ day12genrator_intertor/yeild_num.py
@@ -0,0 +1,9 @@
+# Create a generator that yields even numbers from 0 to n-1
+def fun(n):
+    for x in range(n):
+        if x % 2 == 0:
+            yield x
+
+if __name__ == '__main__':
+    for num in fun(10):
+        print(num)
