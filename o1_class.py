2c94f9e894095e2829a98dad60ecb03e735e218c
naman yadav <naman18gaming@gmail.com>
2026-06-11 22:19:46 +0530

    Day17: Added Student class examples

---
+++ o1_class.py
@@ -0,0 +1,166 @@
+[33m053657e[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m Day17: OOP practice files
+[33m19a798d[m Day17: OOP class example + cleanup old files
+[33m6fdb229[m Day 16: exception handling example
+[33m42a24db[m Ignore Python cache files
+[33mf4e6425[m Complete Day 15 Modules and Packages
+[33ma9fce0f[m Add Day 14 modules and packages practice
+[33m24a808b[m Day 13: welcome decorator example
+[33mec57dfa[m Day 13: start decorator example
+[33m8c006cd[m Day 12: yield characters using generator
+[33ma3d46b4[m Day 12: yield squares using generator
+[33mece7387[m Day 12: yield numbers using generator
+[33m97f058a[m Day 12: iterator examples
+[33ma679294[m Day 12: generator expression practice
+[33m21765da[m Day 12: generator expression examples
+[33m80a62ac[m Day 11: reduce() examples
+[33m8113435[m Day 11: filter list examples
+[33m8452a68[m Day 11: map() conversion examples
+[33m67a316a[m Day 11: find product using Python program
+[33me969828[m Day 11: filter() examples
+[33m638ac93[m Day 11: using map() examples
+[33m2209df7[m Day 10: filter numbers using lambda + filter()
+[33meed6c44[m Day 10: sort students by marks using lambda + sorted
+[33mf4ad1e0[m Day 10: print positive numbers using list comprehension
+[33m040ffcc[m Day 10: word length count program
+[33ma52f68f[m Day 10: temperature conversion using list comprehension
+[33mc104285[m Day 10: map function examples
+[33m212da9c[m Day 10: square comprehension program
+[33m1895837[m Day 10: list comprehension examples
+[33ma6f7c68[m Day 9: Student Record Manager mini project
+[33md276764[m Day 9: JSON print program
+[33me76c15b[m Day 9: JSON file handling program
+[33m1394e99[m Day 9: program to count number of students
+[33m59e5b4e[m Day 9: read file program
+[33ma632227[m Day 9: create CSV file program
+[33m7012458[m Day 9: random choice program
+[33m293252f[m Day 9: factorial module program
+[33m27f3276[m Day 9: modules.py program
+[33mb44b60f[m Day 9: Mini Project 2 - Personal Expense Tracker
+[33m8b78f7a[m Day 9: menu-driven program (Add, Show, Exit)
+[33m95d885e[m Day 9: count characters in file program
+[33m9ae88af[m Day 9: append_file program
+[33m9262262[m Day 9: handle both (write and read file) program
+[33m11b97bc[m Day 9: write_file program
+[33m8f834ed[m Day 9: custom exception handling program
+[33m4f63a41[m Day 9: divide handle program
+[33m477e92d[m Day 9: try_except_print program
+[33m7ed9beb[m Day 8: resuly_func program
+[33ma086670[m Day 8: factorial program
+[33m46ebe03[m Day 8: calculator_fac program
+[33m80ef9dd[m Day 8: maxnum_fun program
+[33mfcb0fd3[m Day 8: sqaure_num program
+[33mc77bc36[m Day 8: print_even_odd program
+[33mac98add[m Day 8: print_markssum program
+[33m53986bd[m Day 8: print_city program
+[33m912475c[m Day 8: sum_infunc program
+[33m4ca513f[m Day 8: parameter_func program
+[33m43fc443[m Day 8: mutiplication_func program
+[33m695cbb0[m Day 8: Added id_function program
+[33m9736da1[m Day 8: Added print_even function
+[33m291cd07[m Day 8: Function to print numbers
+[33ma14ba20[m Day 8: Function to print message
+[33m8a5977e[m Day 7: E-commerce inventory management program
+[33m9c3986b[m Day 7: Unique word counter program
+[33ma49bca5[m Day 7: List to tuple conversion program
+[33m1f404c2[m Day 6: Convert list to set program
+[33m4294672[m Day 7: Character frequency counter program
+[33mf0b95ff[m Day 7: Safe lookup dictionary program
+[33mb12b896[m Day 7: Loop through dictionary program
+[33md47818f[m Day 7: Student dictionary program with add & update
+[33mb5fd1bb[m Day 7: Student dictionary program
+[33mdd6eec5[m Day 7: Difference in sets program
+[33m88bda13[m Day 7: Union & Intersection program
+[33m20973f6[m Day 7: Membership test program
+[33m03528bd[m Day 7: Add/Remove set program
+[33m5700561[m Day 7: Remove duplicate program
+[33m1bb7c91[m Day 7: Replace tuple program
+[33mcff672a[m Day 7: Tuple statistics program
+[33mb3d22ad[m Day 7: Tuple unpack program
+[33mc3a29d1[m Day 7: Tuple print program
+[33m2c750e2[m Day 6: Project2 program
+[33m5e4c45a[m Day 6: Project list program
+[33md9b6cdf[m Day 6: Largest number in list program
+[33mbaed67a[m Day 6: Duplicate elements in list program
+[33md4613e1[m Day 6: Print only even numbers from list program
+[33mfb7709a[m Day 6: Square elements in list program
+[33m0f938a3[m Day 6: Odd and even elements in list program
+[33m94aaf5b[m Day 6: Find maximum and minimum in list program
+[33m1425eac[m Day 6: Average of list elements program
+[33md7368a3[m Day 6: Sum of list elements program
+[33m5d35686[m Day 6: Loop through list program
+[33mcfb3378[m Day 6: Ascending and descending sort program
+[33m0c41d64[m Day 6: Remove element using pop() program
+[33me84465a[m Day 6: Remove element from list program
+[33m1404408[m Day 6: Insert element into list program
+[33mcf8f70c[m Day 6: Append element to list program
+[33m208e936[m Day 6: Replace element in list program
+[33mf5f2ca7[m Day 6: Program to find length of list
+[33m4eba66d[m Day 6: Find element in list program
+[33m02c86f3[m Day 6: Print first and last element of list program
+[33mcaf0dde[m Day 6: Print list program
+[33m4ca696b[m Day 5: Username Formatter program
+[33md8e6733[m Day 5: Password Strength Checker program
+[33m96a5f0f[m Day 5: Program to reverse a string
+[33mdee60a9[m Day 5: Program to count digits and alphabets separately
+[33m3565c8c[m Day 4: Program to find duplicate characters in a string
+[33me8b05e7[m Day 4: Program to count words in a string
+[33ma494376[m Day 4: Program to remove spaces from a string
+[33m68ea068[m Day 4: Program to count frequency of characters in a string
+[33m333744b[m Day 4: Program to check palindrome string
+[33m8a91e74[m Day 4: Program to reverse a string
+[33m8a351f6[m Day 4: Program to print characters of a string using loops
+[33ma585288[m Day 4: Program to count spaces in a string
+[33m101f8a1[m Day 4: Program to count vowels in a string
+[33m1ff0841[m Day 4: Program to replace characters in a string
+[33ma8881b2[m Day 4: Program to convert string to upper and lower case
+[33mc1d851b[m Day 4: Program to find length of a string
+[33mdff5711[m Day 4: Program to print first and last characters of a string
+[33m0cdb98d[m Day 4: String printing program in Python
+[33m4bce171[m Day 3: Password retry system in Python
+[33m4ddad41[m Day 3: ATM menu system (version 2) in Python
+[33m7401fa4[m Day 3: ATM menu system program in Python
+[33m0d8c968[m Day 3: ATM menu system program in Python
+[33m368434a[m day 3 print star pattern in tree form
+[33m6cd40a4[m print quare patern of starts
+[33mcc40f30[m Day 3: Star pattern program in Python
+[33ma6b64b0[m Day 2: Star pattern program in Python
+[33m27d0546[m Day 3: Prime number program in Python
+[33m79af2ca[m Day 3: Count digits in a number program in Python
+[33m86feb04[m Day 3: Factorial program in Python
+[33m6013b8d[m Day 3: Print multiples program in Python
+[33mfa5eb9a[m Day 3: Reverse number printing program in Python
+[33meff4001[m Day 3: Print numbers 1 to 100 program in Python
+[33mda78b7d[m Day 2: Print numbers 1 to 10 program in Python
+[33m0096d26[m Day 2: Random number program in Python
+[33m5e22fea[m Day 2: Number pattern program in Python
+[33m229f154[m Day 2: Star pattern program in Python
+[33md810912[m Day 2: Sum of N natural numbers program in Python
+[33mbef5894[m Day 2: Reverse number program in Python
+[33mf8874dd[m Day 2: Prime number check program in Python
+[33m527fba3[m Day 2: Count digits program in Python
+[33m4189fe7[m Day 2: Armstrong number program in Python
+[33mb6cc286[m Day 2: Fibonacci series program in Python
+[33mc644280[m Day 2: Palindrome check program in Python
+[33md1f0a70[m Day 2: Factorial program in Python
+[33ma1a682a[m Day 2: Multiplication table program in Python
+[33m9a9a942[m Day 2: Print numbers program in Python
+[33m1fb1b78[m Day 2: Print numbers using loops in Python
+[33mace20cd[m Day 1: Bill calculation program in Python
+[33m057504a[m Day 1: Average calculation program in Python
+[33m8aec467[m Day 1: Age program in Python
+[33m845259b[m Day 1: Square and Cube program in Python
+[33m61599d2[m Day 1: Remainder program in Python
+[33m6a5f97e[m Day 1: Calculator program in Python
+[33m89a8578[m Day 1: Student ID Card program in Python
+[33m8b5414e[m Day 1: Minutes to Hours conversion program in Python
+[33m786993c[m Day 1: Simple Interest Calculator in Python
+[33mdf711d0[m Day 1: swap program in Python
+[33maa81ad2[m Day 1: temp program in Python
+[33mfede83c[m Day 1: Square program in Python
+[33m27af2de[m Day 1: Addition program in Python
+[33mc3e757c[m Day 1: Arithmetic practice program
+[33mffcfd82[m Day 1: added intro and hello programs
+[33mb0bb3ee[m Day 1: Python basics practice
+[33me5d1bb3[m Resolved merge conflicts
+[33m6695dd6[m Initial commit: setup repo for 60-day journey
+[33mf6e5910[m Initial commit
