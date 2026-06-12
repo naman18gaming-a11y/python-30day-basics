e4099a7e4d41f1993483d73db5ba237a4f0ab785
naman yadav <naman18gaming@gmail.com>
2026-06-13 03:53:46 +0530

    Day23: Added animal_simulator.py overriding + polymorphism example

---
+++ day23overriding/animal_simulator.py
@@ -0,0 +1,17 @@
+#Animal Sound Simulator
+class animals:
+    def sound(self):
+        print("sounds")
+
+class dog:
+    def sound(self):
+        print("woof")
+
+class cat:
+    def sound(self):
+        print("meow")        
+
+d = dog()
+d.sound()
+c = cat()
+c.sound() 
