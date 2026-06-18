e2f810b08522c0bfd84f3e3a587673af8ade9f1a
naman18gaming-a11y <naman18gaming@gmail.com>
2026-06-18 17:44:50 +0530

    Rock, Paper, Scissors Game

---
+++ day27pythonpractice/practice_set_5.py
@@ -0,0 +1,15 @@
+#rock paper sicssors game
+import random
+choices = ["rock,paper,scissors"]
+user = input("enter rock/paper/scissors")
+comp = random.choice(choices)
+print("comp chooses", comp)
+
+if user == comp:
+    print("Draw!")
+elif (user == "rock" and comp == "scissors") or \
+     (user == "paper" and comp == "rock") or \
+     (user == "scissors" and comp == "paper"):
+    print("You win!")
+else:
+    print("lol lost")
