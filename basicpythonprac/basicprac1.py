6490ec52dd013f4d132bc11651bf26d6d5f2354a
naman18gaming-a11y <naman18gaming@gmail.com>
2026-06-19 23:51:46 +0530

    Given Number is Even or Odd using Recursion

---
+++ basicpythonprac/basicprac1.py
@@ -0,0 +1,6 @@
+# Python Program to Check Whether a Given Number is Even or Odd using Recursion
+num = int(input("Enter the number to check:"))
+if num % 2 == 0 :
+    print("even")
+else:
+    print("odd")
