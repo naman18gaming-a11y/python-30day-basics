abcc0236a79b40303260929df98b54de05b4d203
naman18gaming-a11y <naman18gaming@gmail.com>
2026-06-20 15:24:48 +0530

    Find Prime Numbers in a Given Range

---
+++ basicpythonprac/basicprac14.py
@@ -0,0 +1,9 @@
+#Python Program to Find Prime Numbers in a Given Range
+num = int(input("enter the upper limit:"))
+for a in range(2,num+1):
+    k=0
+    for i in range (2,a//2+1):
+        if(a%i==0):
+            k = k+1
+            if(k<=0):
+                print(a) 
