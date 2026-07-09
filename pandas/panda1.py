f5596a49206c9c665aff1f04b7e457a943fa9feb
naman18gaming-a11y <naman18gaming@gmail.com>
2026-07-09 20:34:25 +0530

    practicing pandaa syntax and problems

---
+++ pandas/panda1.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+marks = pd.Series([95,88,76,90])
+
+print(marks)
---
+++ pandas/panda2.py
@@ -0,0 +1,4 @@
+import pandas as pd
+marks = pd.Series([1,2,3,4,5],index=['a','b','c','d','e'])
+print(marks)
+
