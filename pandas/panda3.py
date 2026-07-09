db5b2d7a5302c7fe5abe79323174e12bf88a3285
naman18gaming-a11y <naman18gaming@gmail.com>
2026-07-09 20:36:04 +0530

    practicing pandaa syntax and problems

---
+++ pandas/panda3.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = {
+     "Name":["Naman","Rahul","Priya"],
+    "Age":[20,21,19],
+    "Marks":[95,88,91]
+}
+df = pd.DataFrame(data)
+print(df)
+print(df.shape)
+print(df.info())
