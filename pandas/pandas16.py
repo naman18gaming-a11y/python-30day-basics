2b4a0cffb36cadedf3d1f69664976830baa1f468
naman18gaming-a11y <naman18gaming@gmail.com>
2026-07-12 21:01:55 +0530

    practicing the numpy basics

---
+++ pandas/pandas16.py
@@ -0,0 +1,12 @@
+import pandas as pd
+data = {
+    "Name": ["Naman", "Rahul", "Priya", "Aman", "Riya", "Arjun"],
+    "City": ["Delhi", "Mumbai", "Delhi", "Pune", "Mumbai", "Delhi"],
+    "Department": ["IT", "HR", "IT", "Finance", "HR", "IT"],
+    "Marks": [95, 88, 91, 76, 84, 79],
+    "Salary": [50000, 45000, 55000, 60000, 47000, 52000]
+}
+
+df= pd.DataFrame(data)
+#print(df)
+print(df.groupby("Department")["Salary"].mean())
