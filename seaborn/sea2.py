d769bef2575a0e2f9a01ea9e8d0377a1434e3a0c
naman18gaming-a11y <naman18gaming@gmail.com>
2026-07-14 19:05:10 +0530

    understanding the concept and syntax

---
+++ seaborn/sea2.py
@@ -0,0 +1,16 @@
+
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+students = pd.DataFrame({
+"Name":["Naman","Rahul","Priya","Aman","Riya"],
+"Marks":[95,82,91,76,88],
+"Hours":[8,6,7,4,6],
+"City":["Delhi","Mumbai","Delhi","Pune","Mumbai"]
+})
+sns.scatterplot(
+    x="Hours",
+    y="Marks",
+    data = students
+)
+plt.show()
