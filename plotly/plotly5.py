c89720857dceb67923c8464a428798d8f2a63f72
naman yadav <naman18gaming@gmail.com>
2026-07-17 18:39:37 +0530

    learning and understanding syntax of plotly

---
+++ plotly/plotly5.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import plotly.express as px
+
+students = pd.DataFrame({
+    "Name":["Naman","Rahul","Priya","Aman","Riya","Arjun","Simran","Karan","Neha","Rohit"],
+    "Marks":[95,82,91,76,88,79,84,93,86,90],
+    "Hours":[8,6,7,4,6,5,6,8,7,7],
+    "City":["Delhi","Mumbai","Delhi","Pune","Mumbai","Delhi","Pune","Delhi","Mumbai","Delhi"],
+    "Gender":["Male","Male","Female","Male","Female","Male","Female","Male","Female","Male"]
+})
+fig = px.box(
+   students,
+    y="Hours",
+    color="City",
+    title="marks distribution by city"
+)
+fig.show()
