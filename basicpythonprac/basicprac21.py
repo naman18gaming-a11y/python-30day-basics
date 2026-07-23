3c0b9ce089999ff839ad822e6f39306b7fc0b596
naman yadav <naman18gaming@gmail.com>
2026-07-23 17:16:21 +0530

    #Python Program to Print the Natural Numbers Summation

---
+++ basicpythonprac/basicprac21.py
@@ -0,0 +1,6 @@
+#Python Program to Print the Natural Numbers Summation 
+n = int(input("Enter a number: "))
+
+summation = n * (n + 1) // 2
+
+print("Summation of first", n, "numbers is:", summation)
