8dd20cf5d93f354282b3f2a9571a8166856b3aa8
naman yadav <naman18gaming@gmail.com>
2026-07-25 13:54:14 +0530

    Delete basicpythonprac/basicprac8.py

--- basicpythonprac/basicprac8.py
+++
@@ -1,6 +0,0 @@
-#Python Program to Find Sum of Digits of a Number
-num = int(input("enter the number: "))
-total_sum = 0
-for numm in str(abs(num)):
-    total_sum += int(numm)
-    print("total sum",total_sum)
