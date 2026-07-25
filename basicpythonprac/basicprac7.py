a9d9fda456de09ff68a29cebd26d792b185ea8ee
naman yadav <naman18gaming@gmail.com>
2026-07-25 13:54:03 +0530

    Delete basicpythonprac/basicprac7.py

--- basicpythonprac/basicprac7.py
+++
@@ -1,5 +0,0 @@
-#Python Program to Print All Numbers in a Range Divisible by a Given Number
-div = int(input("Enter the number to divide: "))
-for num in range(0,51):
-    if div % num == 0:
-        print("NUM: ",)
