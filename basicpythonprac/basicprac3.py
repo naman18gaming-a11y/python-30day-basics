fce7ffba8040798edb637a9eb3ad927c9873d8a5
naman yadav <naman18gaming@gmail.com>
2026-07-25 13:57:10 +0530

    Delete basicpythonprac/basicprac3.py

--- basicpythonprac/basicprac3.py
+++
@@ -1,9 +0,0 @@
-#Python Program to Check if a Number is a Palindrome
-num = int(input("enter the number: "))
-reversed_num =  int(str(num)[::-1])
-if reversed_num == num:
-   print("palindrome")
-else:
-   print("neh")
-
-  
