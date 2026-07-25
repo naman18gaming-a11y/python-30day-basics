fd9290c600ef645bb6723f90bf33a11b2037bff6
naman yadav <naman18gaming@gmail.com>
2026-07-25 13:53:49 +0530

    Delete basicpythonprac/basicprac6.py

--- basicpythonprac/basicprac6.py
+++
@@ -1,4 +0,0 @@
-#Python Program to Find Numbers which are Divisible by 7 and Multiple of 5 in a Given Range
-for num in range(1,100):
-    if num % 7 == 0 and num % 5 == 0 :
-        print("nums are:",num)
