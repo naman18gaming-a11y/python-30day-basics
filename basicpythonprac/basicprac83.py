7752afe148c15a8c16937ecdb903e2237392465a
naman yadav <naman18gaming@gmail.com>
2026-08-01 19:50:44 +0530

    #Python Program to Check if a Key Exists in a Dictionary or Not

---
+++ basicpythonprac/basicprac83.py
@@ -0,0 +1,7 @@
+#Python Program to Check if a Key Exists in a Dictionary or Not
+dic = {'a': 1, 'b': 2, 'c': 3}
+key = input("Enter the key to check: ")
+if key in dic:
+    print("hell yeahhh")
+else:
+    print("helll nawww")
