7437a294dd98b1a289a5ae0800cee8a8ea47eba8
naman yadav <naman18gaming@gmail.com>
2026-08-01 19:48:03 +0530

    #Python Program to Count Occurrences of Element in List

---
+++ basicpythonprac/basicprac78.py
@@ -0,0 +1,7 @@
+#Python Program to Count Occurrences of Element in List
+num = [1, 2, 3, 2, 4, 2, 5]
+x = 2
+if x in  num :
+    count = num.count(x)
+    print(f"The number {x} occurs {count} times in the list.")
+    
