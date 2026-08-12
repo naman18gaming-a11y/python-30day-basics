01830136e0e7ba0f02eb28bd2e25bfac59c12d6e
naman yadav <naman18gaming@gmail.com>
2026-08-12 23:32:19 +0530

    practicing  concept of oops

---
+++ day21oops/prac2.py
@@ -0,0 +1,9 @@
+class car:
+    def vehicle_info(self):
+        print('Inside vehicle class')
+class suv(car):
+    def suv_car_info(self):
+        print('Inside suv class')
+class sports_car(car):
+    def sports_car_info(self):
+        print('Inside sports car class')                
